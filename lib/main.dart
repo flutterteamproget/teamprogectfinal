99a51e86ee228f0d103c189a404e1967e8be8bea
Oh seongui <34715603@naver.com>
2025-12-12 15:13:37 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:teamprogectfinal/view/home.dart';
+import 'package:teamprogectfinal/view/my_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Home(),
+      home: const MyPage(),
     );
   }
 }
