3dfb1ca14d61fceb5f5c96788f230c50cd80bca9
유다원 <pluralize79@gmail.com>
2025-12-12 15:03:27 +0900

    handler 및 model 수정

--- lib/vm/size_category_databasehandler.dart
+++ lib/vm/size_category_databasehandler.dart
@@ -1,5 +1,6 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:teamprogectfinal/model/category_size.dart';
 
 class SizeCategoryDatabasehandler {
 
@@ -9,25 +10,36 @@ class SizeCategoryDatabasehandler {
 int? sc_seq;
   String sc_name;1
 */
-Future<Database> initializeDB() async{
-  String path = await getDatabasesPath();
-  return openDatabase(
-    join(path,'user.db'),
-    onCreate: (db, version) async{
-      await db.execute(
-        """
-        create table size_category
-        (
-        sc_seq integer primary key autoincrement,
-        sc_name text
-                
-        
-        )
+  Future<Database> initializeDB() async{
+    String path = await getDatabasesPath();
+    return openDatabase(
+      join(path,'user.db'),
+      onCreate: (db, version) async{
+        await db.execute(
+          """
+          create table size_category
+          (
+          sc_seq integer primary key autoincrement,
+          sc_name text
+                  
+          
+          )
 
-        """
-      );
-    },
-    version: 1,
-  );
-}
+          """
+        );
+      },
+      version: 1,
+    );
+  }
+
+  Future<List<CategorySize>> queryGender() async{
+    final Database db =await initializeDB();
+    final List<Map<String,Object?>> result = await db.rawQuery(
+      """
+        select * 
+        from size_category
+      """
+    );
+    return result.map((e) => CategorySize.fromMap(e)).toList();
+  }
 }
