d0769a48b119196743d160185977daca04ade76e
Oh seongui <34715603@naver.com>
2025-12-12 14:47:28 +0900

    update
    util - color 추가, view - my page, my update 추가

---
+++ lib/util/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PColor{
+  static Color baseBackgroundColor = Colors.white; // body 배경 컬러
+  static Color appBarBackgroundColor = Colors.white; // appbar 배경 컬러
+
+  static Color primaryColor = Color.fromRGBO(238, 28, 37, 1); // 메인 컬러
+  static Color borderColor = Color.fromRGBO(221, 221, 221, 1); // border, divider 컬러
+
+}
