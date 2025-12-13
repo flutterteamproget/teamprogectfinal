ee943ffb1624210d42eeb0b5ddd42a1d829d353f
Oh seongui <34715603@naver.com>
2025-12-13 16:28:10 +0900

    update
    my page, my update 관련 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:teamprogectfinal/view/home.dart';
-import 'package:teamprogectfinal/view/main_page.dart';
 import 'package:teamprogectfinal/view/my_page.dart';
-import 'package:teamprogectfinal/view/staff_insert_product.dart';
-// import 'package:teamprogectfinal/view/my_update.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
      
-      home: const MainPage(),
+      home: const MyPage(),
     );
   }
 }
