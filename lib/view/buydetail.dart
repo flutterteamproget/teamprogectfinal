a3b960c93f1f4bc1b6319447a7a76830c870de0d
Lim Soyeon <qwerty147704@gmail.com>
2025-12-15 10:33:33 +0900

    mypage, buydbhandler, buydetail 수정

--- lib/view/buydetail.dart
+++ lib/view/buydetail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:teamprogectfinal/util/color.dart';
+import 'package:teamprogectfinal/util/font_size.dart';
 import 'package:teamprogectfinal/vm/buydatabasehandler.dart';
 
 class Buydetail extends StatefulWidget {
@@ -23,6 +25,9 @@ class _BuydetailState extends State<Buydetail> {
   // value[1] = b_date
   // value[2] = b_price
   // value[3] = b_quantity
+  // value[4] = p_image
+  // value[5] = m_name
+  // value[6] = p_size
 
   @override
   void initState() {
@@ -36,49 +41,152 @@ class _BuydetailState extends State<Buydetail> {
     return Scaffold(
       appBar: AppBar(
         title: Text('구매 상세'),
+        backgroundColor: PColor.appBarBackgroundColor,
       ),
+      backgroundColor: PColor.baseBackgroundColor,
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(40.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(value[1].toString().substring(0,10)), // 구매 날짜
-            Text('구매 번호 ${value[0]}'),
-            Divider(
-              indent: 20,
-              endIndent: 20,
+            // 구매 날짜
+            Text(
+              value[1],
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w500
+              ),
             ),
-            Text('방문 수령 정보'),
-            FutureBuilder(
-              future: handler.queryBranchAddress(value[0]), // get argument로 구매번호 불러오기
-              builder: (context, snapshot) {
-                return snapshot.hasData && snapshot.data!.isNotEmpty
-                ? Text('수령 지점 ${snapshot.data!}')
-                : Center(
-                  child: Text('데이터가 없습니다.'));
-              },
+            // 구매 번호
+            Text(
+              '구매번호 ${value[0]}'
             ),
-            Divider(
-              indent: 20,
-              endIndent: 20,
+            SizedBox(
+              height: 10,
             ),
-            Text('주문 상품 ${value[3]}개'),
-            // get argument로 listview
-            Divider(
-              indent: 20,
-              endIndent: 20,
+            Divider(),
+            SizedBox(
+              height: 10,
             ),
-            Text('결제 정보'),
-            FutureBuilder(
-              future: handler.queryProductPrice(value[0]), // get argument로 구매번호 불러오기
-              builder: (context, snapshot) {
-                return snapshot.hasData && snapshot.data!.isNotEmpty
-                ? Text('상품 금액 ${snapshot.data!}원')
-                : Center(
-                  child: Text('데이터가 없습니다.'));
-              },
+            Text(
+              '방문 수령 정보',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500
+              ),
             ),
-            Text('구매 금액 ${value[2]}원')
+            SizedBox(
+              height: 10,
+            ),
+            // 수령할 지점
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '수령 지점',
+                  style: TextStyle(fontSize: 16),
+                ),
+                FutureBuilder(
+                  future: handler.queryBranchAddress(value[0]),
+                  builder: (context, snapshot) {
+                    return snapshot.hasData && snapshot.data!.isNotEmpty
+                    ? Text('${snapshot.data!['br_address']}\n${snapshot.data!['br_name']}')
+                    : Center(
+                      child: Text('데이터가 없습니다.'));
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Divider(),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '주문 상품',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.memory(value[4]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(value[5]),
+                      Text(
+                        '${value[6].substring(1)}',
+                      ),
+                      Text(
+                        '${value[3].toString()}개',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Divider(),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '결제 정보',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.normal
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '상품 금액',
+                  style: TextStyle(fontSize: 16),
+                ),
+                FutureBuilder(
+                  future: handler.queryProductPrice(1, value[0]), // get argument로 구매번호 불러오기, u_seq 필요한지아닌지
+                  builder: (context, snapshot) {
+                    return snapshot.hasData && snapshot.data!.isNotEmpty
+                    ? Text('${snapshot.data!}원')
+                    : Center(
+                      child: Text('데이터가 없습니다.'));
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '구매 금액',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                Text(
+                  '${value[2]}원',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
--- lib/vm/buydatabasehandler.dart
+++ lib/vm/buydatabasehandler.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:teamprogectfinal/model/buy.dart';
@@ -77,12 +80,40 @@ Future<int> insertBuy(Buy buy) async{
 }
 
 
-// key값에 따른 구매데이터 불러오기
-Future<Map<String, Object?>?> queryBuy2(int b_seq) async{
+// 구매내역 
+Future<List> queryBuy2(int u_seq) async{
     final Database db = await initializeDB();
     final List<Map<String, Object?>> queryResults = await db.rawQuery(
       """
-      select * from buy
+      select * from product as p
+      inner join buy as b
+      on p.p_seq = b.p_seq
+      inner join user as u
+      on b.u_seq = u.u_seq
+      where u.u_seq = ?
+      """,
+      [u_seq]
+    ); 
+    print(queryResults);
+    // queryResults = [{b_date : 2025-12-11}], [], ...
+    return queryResults;
+  }
+
+
+
+
+
+
+
+// 수령 지점
+Future<Map?> queryBranchAddress(int b_seq) async{
+    final Database db = await initializeDB();
+    final List<Map<String, Object?>> queryResults = await db.rawQuery(
+      """
+      select br_address, br_name
+      from branch as br
+      inner join buy as b
+      on b.br_seq = br.br_seq
       where b_seq = ?
       """,
       [b_seq]
@@ -95,112 +126,94 @@ Future<Map<String, Object?>?> queryBuy2(int b_seq) async{
     }
   }
 
-// 수령 지점
-// select br_address
-// from branch as br
-// inner join buy as b
-// on b.br_seq = br.br_seq
-Future<String?> queryBranchAddress(int b_seq) async{
-    final Database db = await initializeDB();
-    final List<Map<String, Object?>> queryResults = await db.rawQuery(
-      """
-      select br_address
-      from branch as br
-      inner join buy as b
-      on b.br_seq = br.br_seq
-      where b_seq = ?
-      """,
-      [b_seq]
-    ); 
-    // queryResults = [{b_date : 2025-12-11}], [], ...
-    if(queryResults.isNotEmpty){
-      return queryResults[0]['br_address'].toString();
-    }else{
-      return null;
-    }
-  }
 
-// 구매 수량
-// select b_quantity
-// from buy
-// where b_seq = ?
-Future<String?> queryBuyQuantity(int b_seq) async{
-    final Database db = await initializeDB();
-    final List<Map<String, Object?>> queryResults = await db.rawQuery(
-      """
-      select b_quantity from buy
-      where b_seq = ?
-      """,
-      [b_seq]
-    ); 
-    // queryResults = [{b_date : 2025-12-11}], [], ...
-    if(queryResults.isNotEmpty){
-      return queryResults[0]['b_quantity'].toString();
-    }else{
-      return null;
-    }
-  }
 
-// 구매 상품 보여주기
-// select * from buy
-// where b_seq = ?
-Future<List<Buy>> queryBuyProduct(int b_seq) async{
-    final Database db = await initializeDB();
-    final List<Map<String, Object?>> queryResults = await db.rawQuery(
-      """
-      select * from buy
-      where b_seq = ?
-      """,
-      [b_seq]
-    ); 
-    // queryResults = [{b_date : 2025-12-11}], [], ...
-      return queryResults.map((e) => Buy.fromMap(e)).toList();
-    }
+
 
 // 상품 금액
-// select sum(p_price)
-// from product as p
-// inner join buy as b
-// on b.p_seq = p.p_seq
-// where b_seq = ?
-Future<String?> queryProductPrice(int b_seq) async{
+Future<String?> queryProductPrice(int u_seq, int b_seq) async{
     final Database db = await initializeDB();
     final List<Map<String, Object?>> queryResults = await db.rawQuery(
       """
-      select sum(p_price)
+      select p_price * b_quantity
       from product as p
       inner join buy as b
       on b.p_seq = p.p_seq
       where b_seq = ?
       """,
-      [b_seq]
+      [u_seq, b_seq]
     ); 
     // queryResults = [{b_date : 2025-12-11}], [], ...
     if(queryResults.isNotEmpty){
-      return queryResults[0]['sum(p_price)'].toString();
+      return queryResults[0]['p_price * b_quantity'].toString();
     }else{
       return null;
     }
   }
 
-
-// 결제 금액
-// select b_price from buy
-// where b_seq = ?
-Future<String?> queryBuyPrice(int b_seq) async{
+// 지점별 구매액
+Future<Map?> queryBranchPrice() async{
     final Database db = await initializeDB();
     final List<Map<String, Object?>> queryResults = await db.rawQuery(
       """
-      select b_price from buy
-      where b_seq = ?
+      select br_name, sum(b_price) from branch as br
+      inner join buy as b
+      on br.br_seq = b.br_seq
+	    inner join user as u
+	    on b.u_seq = u.u_seq
+      group by b.br_seq
+      order by sum(b_price) desc
       """,
-      [b_seq]
     ); 
     // queryResults = [{b_date : 2025-12-11}], [], ...
     if(queryResults.isNotEmpty){
-      return queryResults[0]['b_price'].toString();
+      return queryResults[0];
     }else{
       return null;
     }
   }
+
+  // 가장 많이 방문한 지점, 방문 횟수
+  Future<Map?> queryBranchVisit() async{
+    final Database db = await initializeDB();
+    final List<Map<String, Object?>> queryResults = await db.rawQuery(
+      """
+      select br_name, count(b.br_seq) from branch as br
+      inner join buy as b
+      on b.br_seq = br.br_seq
+      group by b.br_seq
+      order by count(b.br_seq) desc
+      """,
+    ); 
+    // queryResults = [{b_date : 2025-12-11}], [], ...
+    if(queryResults.isNotEmpty){
+      return queryResults[0];
+    }else{
+      return null;
+    }
+  }
+
+  // 자주 이용한 브랜드
+  Future<List<Map<String, Object?>>?> queryFavoriteMaker() async{
+    final Database db = await initializeDB();
+    final List<Map<String, Object?>> queryResults = await db.rawQuery(
+      """
+      select m_name, count(m.m_seq) from maker as m
+      inner join product as p
+      on m.m_seq = p.m_seq
+      inner join buy as b
+      on b.p_seq = p.p_seq
+      group by m.m_seq
+      order by count(m.m_seq) DESC
+      """,
+    ); 
+    // queryResults = [{b_date : 2025-12-11}], [], ...
+    if(queryResults.isNotEmpty){
+      return queryResults;
+    }else{
+      return null;
+    }
+  }
+
+
 }
